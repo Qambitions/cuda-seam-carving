87d068055ce618268a7abba903734b49666fbb8a
Ngo Kiet <nvakiet191@gmail.com>
2023-01-09 10:41:57 +0700

    fix PNM file reader and writer functions

---
+++ cuda_code/sources/utils.cu
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Utility function to check for CUDA return errors
+#define CHECK(call)\
+{\
+	const cudaError_t error = call;\
+	if (error != cudaSuccess)\
+	{\
+		fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+		fprintf(stderr, "code: %d, reason: %s\n", error,\
+				cudaGetErrorString(error));\
+		exit(EXIT_FAILURE);\
+	}\
+}
+
+// A class to count the elapsed running time
+struct GpuTimer
+{
+	cudaEvent_t start;
+	cudaEvent_t stop;
+
+	GpuTimer()
+	{
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+	}
+
+	~GpuTimer()
+	{
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
+	}
+
+	void Start()
+	{
+		cudaEventRecord(start, 0);                                                                 
+		cudaEventSynchronize(start);
+	}
+
+	void Stop()
+	{
+		cudaEventRecord(stop, 0);
+	}
+
+	float Elapsed()
+	{
+		float elapsed;
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&elapsed, start, stop);
+		return elapsed;
+	}
+};
+
+
+// Functions to read & write PNM image file
+void readPnm(char * fileName, int &numChannels, int &width, int &height, uchar3 * &pixels)
+{
+	FILE * f = fopen(fileName, "rb");
+	if (f == NULL) {
+		printf("Cannot read %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+	char type[3];
+	fscanf(f, "%s", type);
+	if (strcmp(type, "P6") == 0) {
+		numChannels = 3;
+	} else
+	{
+		printf("PPM file flag is not P6\n"); 
+		fclose(f);
+		printf("Cannot read %s\n", fileName); 
+		exit(EXIT_FAILURE); 
+	}
+
+	fscanf(f, "%i", &width);
+	fscanf(f, "%i", &height);
+
+	int max_val;
+	fscanf(f, "%i", &max_val);
+	if (max_val > 255) // We assume 1 byte per value
+	{
+		printf("Only 256-color (8 bit-per-channel-pixel) images are allowed\n");
+		fclose(f);
+		printf("Cannot read %s\n", fileName); 
+		exit(EXIT_FAILURE); 
+	}
+
+	pixels = (uchar3 *)malloc(width * height * sizeof(uchar3));
+	fread(pixels, sizeof(uchar3), width * height, f);
+	fclose(f);
+}
+
+void writePnm(void* pixels, int numChannels, int width, int height, 
+		char * fileName)
+{
+	FILE * f = fopen(fileName, "wb");
+	if (f == NULL)
+	{
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}	
+
+	if (numChannels == 1)
+		fprintf(f, "P2\n");
+	else if (numChannels == 3)
+		fprintf(f, "P6\n");
+	else
+	{
+		fclose(f);
+		printf("Cannot write %s\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+	fprintf(f, "%i %i\n255", width, height);
+	if (numChannels == 1) {
+		fprintf(f, "\n");
+		for (int i = 0; i < width * height * numChannels; i++)
+			fprintf(f, "%hhu\n", ((uint8_t*) pixels)[i]);
+	} else if (numChannels == 3) {
+		fwrite(pixels, sizeof(uchar3) * width, height, f);
+	}
+	fclose(f);
+}
+
+// Function to check if a string startwith a substring
+bool StartsWith(const char *a, const char *b) {
+   if(strncmp(a, b, strlen(b)) == 0) return 1;
+   return 0;
+}
